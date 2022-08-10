60ac7ef44c9b1d7c146f076fbdee5c84046a45e9
sondv-1248 <dao.vinh.son@sun-asterisk.com>
2022-08-10 13:41:07 +0700

    To do list apply props

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,65 @@
 <template>
   <section>
-    <div class="head">
-
-    </div>
-    <ul>
-      <to-do-list></to-do-list>
-    </ul>
+    <header>
+      <div class="add-new">
+        <p><strong>New Todo:</strong></p>
+        <input class="form-control" />
+        <button class="btn">Add</button>
+      </div>
+      <div class="list">
+        <div class="edit">
+          <h4><strong>Edit:</strong></h4>
+          <input class="form-control" />
+          <button class="btn">Save</button>
+        </div>
+        <ul>
+          <to-do-list
+            v-for="item in items"
+            :key="item.id"
+            :name="item.name"
+            :checked="item.checked"
+          >
+          </to-do-list>
+        </ul>
+      </div>
+    </header>
   </section>
 </template>
 
 <script>
-
+export default {
+  data() {
+    return {
+      items: [
+        {
+          id: 1,
+          name: "Eat",
+          checked: true,
+        },
+        {
+          id: 2,
+          name: "Play",
+          checked: false,
+        },
+        {
+          id: 3,
+          name: "Sleep",
+          checked: true,
+        },
+        {
+          id: 4,
+          name: "Shopping",
+          checked: false,
+        },
+        {
+          id: 5,
+          name: "Running",
+          checked: false,
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
@@ -40,6 +89,16 @@ header {
 #app .head {
   background: #58004d;
   padding: 100px;
+}
+
+#app .add-new {
+  font-size: 20px;
+  color: #6e7fe0;
+  background: #fff;
+  padding: 0 0 20px 0;
+  border: 1px solid #fff;
+  border-radius: 5px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
 }
 
 #app ul {
@@ -80,5 +139,116 @@ header {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
+#app ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+#app li {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app h4 {
+  text-align: left;
+  margin-left: 125px;
+}
+
+#app .add-new p {
+  text-align: left;
+  margin-left: 125px;
+}
+
+#app .add-new .form-control {
+  font-size: 20px;
+  border: 1px #6e7fe0 solid;
+}
+
+#app .add-new .btn {
+  margin-left: 10px;
+  border-radius: 5px;
+  padding: 6px 20px;
+  font-size: 15px;
+}
+
+#app .list {
+  margin-top: -50px;
+  font-size: 20px;
+  color: #6e7fe0;
+  background: #fff;
+  padding: 0 0 20px 0;
+  border: 1px solid #fff;
+  border-radius: 5px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+}
+
+#app .list p {
+  text-align: left;
+  margin-left: 125px;
+  width: 60%;
+  position: absolute;
+}
+
+#app .list .form-control {
+  font-size: 20px;
+  border: 1px #6e7fe0 solid;
+}
+
+#app .list .btn {
+  margin-left: 10px;
+  border-radius: 5px;
+  padding: 6px 20px;
+  font-size: 15px;
+}
+
+#app .list .btn-edit {
+  margin-left: 340px;
+  position: relative;
+}
+
+#app .list .btn-delete {
+  margin-left: 10px;
+  position: relative;
+}
+
+#app .list .edit {
+  padding-bottom: 20px;
+  border-bottom: 3px solid #f1f1f1;
+}
+
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+
+#app button:hover,
+#app button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
+.active {
+  background: #98dfbf;
+}
+
+#app .list p {
+    text-align: left;
+    margin-left: 125px;
+    width: 60%;
+    position: absolute;
+    margin-top: -29px;
 }
 </style>
